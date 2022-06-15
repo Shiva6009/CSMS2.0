3446eb7810ff1b985e1cbdd39e89d4e348a86038
Siva <logasivasubramani@gmail.com>
2022-06-16 00:17:10 +0530

    Try to do Login Verfiction between NodeJs to MongoDB

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.post('/', function (req, res, next) {
   console.log(" Inside Router")
   console.log(" UserName ", req.body.employeeID)
   console.log(" Password ", req.body.password)
-  res.send(" Hai Node ")
+//  res.send(" Hai Node ")
   mongoose.connect("mongodb+srv://Shiva:Admin123@cluster0.se2xkky.mongodb.net/?retryWrites=true&w=majority",
     {
       useNewUrlParser: true, useUnifiedTopology: true
@@ -19,9 +19,22 @@ router.post('/', function (req, res, next) {
       else {
         //console.log(" DB Connected ")
         results = 'DB Connected'
+
+        var collection = mongoose.Collection('LoginDetails')
+        collection.find({ emailid: req.body.employeeID }, { $exists: true }).toArray(function (err, docs) //find if documents that satisfy the criteria exist
+        {
+          if (docs.length > 0) //if exists
+          {
+            console.log(docs); // print out what it sends back
+          }
+          else // if it does not 
+          {
+            console.log("Not in docs");
+          }
+        });
         console.log(" Results ", results)
-        response.setHeader("Content-Type", "text/html")
-        res.send(results)
+     //   response.setHeader("Content-Type", "text/html")
+       // res.send(results)
       }
     })
 
